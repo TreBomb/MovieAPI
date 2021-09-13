7ce00604d8bbc2ceffe3a6d537178bab91b9643e
Trestin <trestinishak@gmail.com>
2021-09-13 13:13:41 -0500

    search bar

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,7 +4,7 @@ function MovieCard ({ movie }) {
     const BASE_URL = "https://image.tmdb.org/t/p/w500/";
 
     return(
-        <img src={`${BASE_URL}${movie.poster_path}`} alt="movie poster"></img>
+        <img src={`${BASE_URL}${movie.poster_path}`} alt={`${movie.title} Poster`}></img>
     );
 }
 
